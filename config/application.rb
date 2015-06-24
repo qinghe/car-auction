25e757e6ccc754eed7dcc21aad50c82b587b95f8
qing he <areq22@aliyun.com>
2015-06-24 07:19:35 -0700

    add assets to precompile

--- config/application.rb
+++ config/application.rb
@@ -47,7 +47,7 @@ module Inz
     
     # Enable the asset pipeline
     config.assets.enabled = true
-
+    config.assets.precompile += ['frontend.js', 'admin.js', 'backend.js']
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
 
