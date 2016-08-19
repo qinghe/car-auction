2a2c775baa38adc58d529d22fb11c51763ba594b
qinghe <areq22@gmail.com>
2016-08-19 19:14:14 +0800

    for debug, remove bidderList prams

--- app/models/pingan/auction_result_message.rb
+++ app/models/pingan/auction_result_message.rb
@@ -3,7 +3,7 @@ module Pingan
     self.required_fields = [ :partnerAccount, :taskAuctionNo, :announcementStartTime,:announcementEndTime,
       :auctionLocation, :startTime, :endTime, :auctionType, :isPass, :passTimes,
       :commissionedTime, :transferComplete, :transferRequestTime, :transferRealTime,
-      :finalPrice, :bidTimes, :isApplyInquire, :biddersList ]
+      :finalPrice, :bidTimes, :isApplyInquire ]#, :biddersList
 
     #ANNOUNCEMENT_TIME    拍卖公示时间      auction.public_start_at
     #AUCTION_LOCATION     拍卖地点       N  auction.location
